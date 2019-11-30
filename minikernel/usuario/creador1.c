325ae5b821c39f009de793c3c248c4afcff3681c
Oscar Nydza <oscar.nydza@protonmail.com>
2019-11-30 13:01:54 +0100

    Añadidos retornos de carro en funciones de creacion para facilitar la depuración

--- minikernel/usuario/creador1.c
+++ minikernel/usuario/creador1.c
@@ -19,19 +19,19 @@ int main(){
 
 	if (crear_mutex("m1", NO_RECURSIVO)<0)
 		printf("error creando m1. NO DEBE SALIR\n");
-
+		printf("\n");
 	if (crear_mutex("m1", NO_RECURSIVO)<0)
 		printf("error creando m1. DEBE SALIR\n");
-
+		printf("\n");
 	if (crear_mutex("m2", NO_RECURSIVO)<0)
 		printf("error creando m2. NO DEBE SALIR\n");
-
+		printf("\n");
 	if (crear_mutex("m3", NO_RECURSIVO)<0)
 		printf("error creando m3. NO DEBE SALIR\n");
-
+		printf("\n");
 	if (crear_mutex("m4", NO_RECURSIVO)<0)
 		printf("error creando m4. NO DEBE SALIR\n");
-
+		printf("\n");
 	printf("creador1 duerme 1 segundo\n");
 	dormir(1);
 
